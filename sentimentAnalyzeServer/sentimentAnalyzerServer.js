183343a234753ff450738f05073c4c71689220e6
kevinmbuicode <kevinmushan@gmail.com>
2021-06-20 09:20:47 +0000

    updated sentimentAnalyzer with IBM

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,33 +1,20 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(express.static('client'))
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
 
-const cors_app = require('cors');
-app.use(cors_app());
+    const NaturalLanguageUnderstaningV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
 
-app.get("/",(req,res)=>{
-    res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
-});
-
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
-});
-
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
-});
-
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
-});
-
-let server = app.listen(8080, () => {
-    console.log('Listening', server.address().port)
-})
-
+    const naturalLanguageUnderstaning = NaturalLanguageUnderstaningV1({
+        version: '2021-06-20',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceURL: api_url,
+    });
+    return naturalLanguageUnderstaning;
+}
